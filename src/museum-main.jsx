a465cf42aea44126293944e83ef69bfa05de22dc
ghollisjr <ghollisjr@gmail.com>
2026-03-14 17:59:13 -0400

    converted to vite react app

---
+++ src/museum-main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WanderingMuseum from '../wandering-museum-3d.jsx';
+import './museum-main.css';
+
+function App() {
+  const handleComplete = (results) => {
+    console.log('Museum completed!', results);
+    alert(
+      `Museum Complete!\nMethod: ${results.completionMethod}\nOpenness Score: ${Math.round(results.abstractnessLevel * 100)}%`
+    );
+
+    // Send to parent if in iframe
+    if (window.parent !== window) {
+      window.parent.postMessage({ type: 'MUSEUM_COMPLETE', data: results }, '*');
+    }
+  };
+
+  return <WanderingMuseum onComplete={handleComplete} />;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
